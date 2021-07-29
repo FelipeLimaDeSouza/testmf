3caec979a0fa2fdc409e651568f03dd5ab52172b
Felipe Lima <felipe.limasouza99@gmail.com>
2021-07-29 20:54:30 -0300

    change version

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,9 +1,9 @@
 declare module 'test-microfrontend-react-native' {
-        interface Props {
-            fileName?: string
-        }
+    interface Props {
+        fileName?: string
+    }
 
-        const TestMF: (props: Props) => JSX.Element
-        
-        export default TestMF
+    const TestMF: (props: Props) => JSX.Element
+    
+    export default TestMF
 }
